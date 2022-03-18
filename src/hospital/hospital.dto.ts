a57f0eb4ce225fdccdacc7d5f66dbbc443df1ef6
Caio Poyares <capoyares@gmail.com>
2022-03-17 21:59:55 -0300

    change update hospital flow

--- src/hospital/hospital.dto.ts
+++ src/hospital/hospital.dto.ts
@@ -1,27 +1,43 @@
 import {
-    IsNotEmpty,
-    IsNumberString,
-    IsString,
-    MaxLength,
-    MinLength,
-  } from 'class-validator';
-  
-  export class CreateHospitalDto {
-    constructor(){
-    }
-  
-    @IsNotEmpty()
-    @IsString()
-    name: string;
-  
-    @IsNotEmpty()
-    @IsString()
-    address: string;
-  
-    @IsNotEmpty()
-    @IsNumberString()
-    @MinLength(10)
-    @MaxLength(13)
-    telephone: string;
-  }
-  
+  IsNotEmpty,
+  IsNumberString,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateHospitalDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  address: string;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  @MinLength(10)
+  @MaxLength(13)
+  telephone: string;
+}
+
+export class UpdateHospitalDto {
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  address: string;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  @MinLength(10)
+  @MaxLength(13)
+  @IsOptional()
+  telephone: string;
+}
